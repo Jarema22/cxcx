d6d9e491a2f518605691e4b0a03a0dcd18891ae3
stranger <tkpanio@gmail.com>
2024-09-01 10:28:08 +0200

    commit 2  01.09.2024 10:28

--- .venv/main_portfolio_web.py
+++ .venv/main_portfolio_web.py
@@ -1,5 +1,9 @@
 import streamlit as st
+import pandas
+
 from pygments.styles.dracula import yellow
+from streamlit import image, empty
+
 st.set_page_config(layout="wide")
 col1 ,col2 =st.columns(2)
 
@@ -10,4 +14,26 @@ with col2:
     content = """
     I am Just NOOB
     """
+
     st.info(content)
+
+content1 = "Below you can finde some of the apps I have built in Python. Feel free to contact me!"""
+st.write(content1)
+
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
+
+df = pandas.read_csv("/home/stranger/Documents/python/app1/pythonProject1/.venv/data (2).csv", sep=";")
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+        st.write(row['description'])
+        st.image("/home/stranger/Documents/python/app1/pythonProject1/images/" + row['image'])
+        st.write(f"[Source Code]({row['url']})")
+
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+        st.write(row['description'])
+        st.image("/home/stranger/Documents/python/app1/pythonProject1/images/" + row['image'])
+        st.write(f"[Source Code]({row['url']})")
